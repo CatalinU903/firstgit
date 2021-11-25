2f0791483350342e9ab96708e7d00548ae163031
CatalinU903 <95039785+CatalinU903@users.noreply.github.com>
2021-11-25 21:05:03 +0200

    Modificari curs 4 working with strings

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace ConsoleApp1
 {
@@ -7,6 +9,49 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Udrescu CatalinS");
+            string myString = "Hello";
+            string myNextStrinh = "Worlfd";
+
+            string a = "a";
+            string b = "b";
+            string c = "c";
+
+            string random = "B";
+
+            int rezultleft = string.Compare(a, b);
+            int rezultlright = string.Compare(c, b);
+            int rezultequal = string.Compare(random, b, false);
+
+
+            //Concat
+
+            myString = myString + "world"; // + concateneaza
+            string helloWorld = myString + " " + myNextStrinh;
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+         
+            for (int i = 0; i < 100000; i++)
+            {
+                myString += i; //  myString = myString + 1
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);
+
+            Stopwatch stopWatch1 = Stopwatch.StartNew();
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append(myString);
+            for (int i = 0; i < 100000; i++)
+            {
+                builder.Append(i);
+            }
+
+            stopWatch1.Stop();
+            Console.WriteLine(stopWatch1.Elapsed);
+
+
+
         }
     }
 }
